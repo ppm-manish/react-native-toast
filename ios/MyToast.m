c784271db4661e995b63af64cd56b34b2209e099
ppm-manish <manish.kumar@paypermint.in>
2020-01-09 12:48:15 +0530

    added payabbhi ios sdk

--- ios/MyToast.m
+++ ios/MyToast.m
@@ -2,6 +2,8 @@
 #import "IOSNativeToast.h"
 #import <React/RCTLog.h>
 
+#import <Payabbhi/Payabbhi-Swift.h>
+
 @interface MyToast()
 @property (nonatomic, retain) IOSNativeToast *toast;
 @end
@@ -23,10 +25,23 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(show:(NSString *)text)
+RCT_EXPORT_METHOD(open:(NSDictionary *)options)
 {
-    RCTLogInfo(@"Pretending to create an event %@", text);
+    
+    RCTLogInfo(@"Pretending to create an event %@", options);
+    
+    NSString *accessID = (NSString *)[options objectForKey:@"access_id"];
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        Payabbhi *payabbhi = [[Payabbhi alloc] initWithAccessID: accessID delegate:self];
+        
+        PayabbhiError *error = [payabbhi openWithOptions: options];
+        
+        RCTLogInfo(@"ERROR: Message: %@,  Code: %ld", error.getMessage, error.getCode);
+    });
+    
 //  [self.toast showToast:text];
+    
+    
 }
 
 @end
---
+++ ios/Payabbhi.framework/Headers/Payabbhi.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Payabbhi.
+FOUNDATION_EXPORT double PayabbhiVersionNumber;
+
+//! Project version string for Payabbhi.
+FOUNDATION_EXPORT const unsigned char PayabbhiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Payabbhi/PublicHeader.h>
+
+
